e9bff8a4f1582a77c1eab3cd818ad0f29d22d46f
Brent Pedersen <bpederse@gmail.com>
2014-09-06 07:25:39 -0600

    install

--- .install-bedtools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=${TRAVIS_BUILD_DIR}
-BRANCH={v2.20.0}
-git clone https://github.com/arq5x/bedtools2.git ${DIR}/bedtools2
-cd ${DIR}/bedtools2
-git checkout $BRANCH
-make
---
+++ .install-htslib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DIR=${TRAVIS_BUILD_DIR}
+BRANCH=develop
+git clone https://github.com/samtools/htslib.git ${DIR}/htslib
+cd ${DIR}/htslib
+git checkout $BRANCH
+make
