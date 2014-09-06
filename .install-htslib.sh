de6a6ea7cecec5f3ca467572e6c46b1b781fae9b
Brent Pedersen <bpederse@gmail.com>
2014-09-06 11:11:40 -0600

    include

--- .install-htslib.sh
+++ .install-htslib.sh
@@ -5,9 +5,9 @@ git clone https://github.com/samtools/htslib.git ${DIR}/htslib
 cd ${DIR}/htslib
 git checkout develop
 
-make
+sudo make install
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
-export C_INCLUDE_PATH=$(pwd):${DIR}
+export C_INCLUDE_PATH=$(pwd):${DIR}:/usr/local/include/:/usr/include/
 
 sudo ldconfig
