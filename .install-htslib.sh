3612cda0b19a3808a0575221249527cc0b19607a
Brent Pedersen <bpederse@gmail.com>
2014-09-06 07:52:35 -0600

    LD_LIB?

--- .install-htslib.sh
+++ .install-htslib.sh
@@ -5,4 +5,5 @@ git clone https://github.com/samtools/htslib.git ${DIR}/htslib
 cd ${DIR}/htslib
 git checkout $BRANCH
 sudo make install
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
 sudo ldconfig
