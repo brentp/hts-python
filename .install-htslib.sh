9c14a98d0e6efec54a7876d6f4a1b46e72fed978
Brent Pedersen <bpederse@gmail.com>
2014-09-06 08:10:46 -0600

    last guess

--- .install-htslib.sh
+++ .install-htslib.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -x
 DIR=${TRAVIS_BUILD_DIR}
-BRANCH=develop
 git clone https://github.com/samtools/htslib.git ${DIR}/htslib
 cd ${DIR}/htslib
-git checkout $BRANCH
+git checkout develop
+
 make
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:$(pwd)
+
 sudo ldconfig
